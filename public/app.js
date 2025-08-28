25fd6fe481ffc97f1b643f1e5d37947af2069019
Isaac Wu <isaacwu2000@gmail.com>
2025-08-28 13:21:45 -0400

    finished page change with auth

--- public/app.js
+++ public/app.js
@@ -1,33 +1,34 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js'
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js'
+import { onAuthStateChanged, signInWithPopup } from './firebaseConfig.js';
+import { app, auth, db, signOut, provider } from './firebaseConfig.js';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBXHfzw5VPOumy-y69luQPwOCo2opnAJ3Q",
-    authDomain: "megablunder-masters.firebaseapp.com",
-    projectId: "megablunder-masters",
-    storageBucket: "megablunder-masters.firebasestorage.app",
-    messagingSenderId: "634759769094",
-    appId: "1:634759769094:web:a26ab05de78391e0c9a787",
-    measurementId: "G-R0LKMXKGTX"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
-
+const appPage = document.getElementById('app');
 const getStartedBtnLarge = document.getElementById('getStartedBtnLarge');
 const signInBtn = document.getElementById('signInBtn');
 const getStartedBtnSmall = document.getElementById('getStartedBtnSmall');
+let signOutBtn;
+
+async function loadPage(page) {
+    const result = await fetch(page);
+    const html = await result.text();
+    appPage.innerHTML = html;
+}
+
+function initApp() {
+    signOutBtn = document.getElementById('signOutBtn');
+    signOutBtn.onclick = async () => {
+        await signOut(auth);
+        await loadPage('index.html');
+    }
+}
 
 getStartedBtnLarge.onclick = () => signInWithPopup(auth, provider)
 
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async (user) => {
     if (user) {
         console.log(user);
         const uid = user.uid;
+        await loadPage('app.html');
+        initApp();
     } else {
         console.log('signed out');
     }
--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,1 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
+export { onAuthStateChanged, signOut, signInWithPopup } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBXHfzw5VPOumy-y69luQPwOCo2opnAJ3Q",
+    authDomain: "megablunder-masters.firebaseapp.com",
+    projectId: "megablunder-masters",
+    storageBucket: "megablunder-masters.firebasestorage.app",
+    messagingSenderId: "634759769094",
+    appId: "1:634759769094:web:a26ab05de78391e0c9a787",
+    measurementId: "G-R0LKMXKGTX"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
